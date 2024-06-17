a9ba905f87ddff7b459dfc747fe5dfe1030ce004
Aarchishya Kapoor <aarchishya@gmail.com>
2024-06-17 16:13:53 +0530

    string slicing added

--- Day11.py
+++ Day11.py
@@ -6,3 +6,11 @@ print(a[2])
 for character in a:
   print(character)
 
+names= "John,Doe"
+print(names[0:4])
+print(len(names))
+print(names[:4])
+print(names[0:-2])
+print(names[0:len(names)-2])
+print(names[-1:len(names)-2])
+print(names[:])
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+a="notinterested"
+# print(a[0])
+# print(a[1])
+# print(a[2])
+
+# for character in a:
+#   print(character)
